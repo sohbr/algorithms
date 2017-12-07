9c4e14c0609cb69a81c3003676d63f4c348e6fbb
sohbr <sohnbrian@gmail.com>
2017-12-07 12:46:04 -0800

    add correction

--- inverted_binary_tree.rb
+++ inverted_binary_tree.rb
@@ -1,8 +1,8 @@
 def invert_binary_tree(root)
   return if root == nil
-  
-  root.left = invert_binary_tree(root.right)
-  root.right = invert_binary_tree(root.left)
-  
+
+  root.left, root.right = invert_binary_tree(root.right),
+                          invert_binary_tree(root.left)
+
   root
 end
