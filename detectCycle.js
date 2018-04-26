5b0e435783b345f9180d19ecaaa3cc46ecb7bc3b
sohbr <sohnbrian@gmail.com>
2018-04-26 15:09:39 -0700

    detect cycle in linked list

---
+++ detectCycle.js
@@ -0,0 +1,33 @@
+function listNode(val) {
+  this.val = val
+  this.next = null
+}
+
+var detectCycle = function(head) {
+  let slow = head,
+    fast = head
+
+  while (fast) {
+    slow = slow.next
+    fast = fast.next
+    if (fast !== null) fast = fast.next
+    if (fast === slow) {
+      slow = head
+      while (slow !== fast) {
+        slow = slow.next
+        fast = fast.next
+      }
+      return slow
+    }
+  }
+
+  return null
+}
+
+let a = new listNode(1)
+let b = new listNode(2)
+
+a.next = b
+b.next = a
+
+console.log(detectCycle(a))
